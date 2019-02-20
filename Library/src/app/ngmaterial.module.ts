441391bcffc2bbf911a75610ed8540e8a91d9279
Хамелеон <maxali946@gmail.com>
2019-02-20 18:08:32 +0400

    BooksInput

--- Library/src/app/ngmaterial.module.ts
+++ Library/src/app/ngmaterial.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
 imports: [
   MatButtonModule,
   MatToolbarModule,
-  MatSelectModule
+  MatSelectModule,
+  MatFormFieldModule,
+  MatInputModule
 ],
 exports: [
   MatButtonModule,
   MatToolbarModule,
-  MatSelectModule
+  MatSelectModule,
+  MatFormFieldModule,
+  MatInputModule
 ]
 })
 export class MaterialAppModule { }
